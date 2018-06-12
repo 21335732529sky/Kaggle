8a27d97d9b6dbc316435d8afa747b1dac04c23d0
OWNER-PC\空 <u271969h@gmail.com>
2018-06-12 20:34:11 +0900

    Update Dataset.py

--- HomeCredit/Dataset.py
+++ HomeCredit/Dataset.py
@@ -1,30 +1,58 @@
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import MinMaxScaler
+from AutoEncoder import AutoEncoder
+import sys
 
 class Dataset:
-    def __init__(self, path_train, path_test, omit=[], target='', validate_size=0.2):
-        self.encoder = {}
-        self.train = self._read_data(path_train, target_col=target, omit=omit[0])
-        self.test = self._read_data(path_test, istest=True, omit=omit[1])
-        self.split = int(self.train[0].shape[0]*validate_size)
+    def __init__(self, path_train='', path_test='', norm_func=None,
+                 omit=[], target='', additional=[], validate_size=0.2):
+        self.encoders = {}
+        self.train = self._read_data(path_train, target_col=target, omit=omit[0]) if path_train != '' else None
+        self.test = self._read_data(path_test, istest=True, omit=omit[1]) if path_test != '' else None
+        self.split = int(self.train[0].shape[0]*(1 - validate_size)) if self.train != None else None
+
+        if self.train is not None: self.train[0] = self._normalize(self.train[0], omit=omit)
+        if self.test is not None: self.test = self._normalize(self.test, omit=['SK_ID_CURR'])
+
+    def _merge(self, base, add, on=''):
+        df = pd.DataFrame(columns=[on] + list(range(add.shape[0]-1)))
+        for i, index in enumerate(add.ix[:, [on]].values.flatten()):
+            tmp = sum(v for v in add.ix[add.ix[:, [on]] == i, :].values.flatten()) / (add.index == i).shape[0]
+            df[i] = np.array([index] + list(tmp))
+
+        return pd.merge(base, add, on=[on])
+
+    def _compress(self, df, index=''):
+        ae = AutoEncoder(df.shape[0], int(df.shape[0]/2))
+        ae.train(df.values, 2, 20, verbose=True)
+
+        return pd.DataFrame(ae.encode(df.values.ix[:, df.columns != index]), index=index)
 
     def _impute(self, df):
-        if len(self.encoder.keys()) == 0:
-            for key in df.ix[:, df.dtypes == 'object'].columns:
-                self.encoder[key] = LabelEncoder().fit(df[key].fillna('N').values.flatten())
-
         for key in df.ix[:, df.dtypes == 'object'].columns:
-            df[key] = self.encoder[key].transform(df[key].fillna('N').values.flatten())
+            try:
+                df[key] = self.encoders[key].transform(df[key].fillna('N').values)
+            except KeyError:
+                self.encoders[key] = LabelEncoder().fit(df[key].fillna('N').values)
+                df[key] = self.encoders[key].transform(df[key].fillna('N').values)
+
+        return df
+
+    def _normalize(self, df, omit=[]):
+        for key in df.columns:
+            if key not in omit: df[key] = MinMaxScaler().fit_transform(df[key].values)
 
         return df
 
 
-    def _read_data(self, path, target_col='', istest=False, omit=[]):
+    def _read_data(self, path, target_col='', istest=False, omit=[], target=''):
         df = self._impute(pd.read_csv(path))
-
+        if target_col == '': istest = True
         df = df.fillna(0)
         columns = [x not in omit for x in df.columns]
-        return (df.ix[:, columns], df.ix[:, [target_col]])\
+        return [df.ix[:, columns], df.ix[:, [target_col]]]\
                if not istest else df.ix[:, columns]
 
     def train_data(self):
