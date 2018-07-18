c8f787ca95700deb8d6b77939da436dba35e638c
Onizuka Lab <onizukalab@OnizukanoMacBook-ea.local>
2018-07-18 22:15:56 +0900

    fix searcher

--- HomeCredit/Searcher.py
+++ HomeCredit/Searcher.py
@@ -7,6 +7,15 @@ from time import sleep
 from multiprocessing import Process, cpu_count
 running = 0
 n_cpu = cpu_count()
+filepath = "/home/u271969h/.kaggle/competitions/home-credit-default-risk/"
+additional_data = [{'path': filepath + 'bureau.csv', 'useAE': True,
+                    'index': 'SK_ID_CURR', 'omit': ['SK_ID_BUREAU']},
+                   {'path': filepath + 'previous_application.csv', 'useAE': False,
+                    'index': 'SK_ID_CURR', 'omit': ['SK_ID_PREV']},
+                   {'path': filepath + 'POS_CASH_balance.csv', 'useAE': False,
+                    'index': 'SK_ID_CURR', 'omit': ['SK_ID_PREV']},
+                   {'path': filepath + 'installments_payments.csv', 'useAE': False,
+                    'index': 'SK_ID_CURR', 'omit': ['SK_ID_PREV']}]
 
 class Searcher:
     def __init__(self, search_space):
@@ -54,8 +63,10 @@ if __name__ == '__main__':
                  'max_depth': [2, 4, 6, 8, 10],
                  'min_samples_split': [2, 4, 8, 16, 32],
                  'learning_rate': [1e-4, 1e-3, 1e-2, 1e-1]})
-    d = Dataset('D:HomeCredit/application_train.csv',
-                'D:HomeCredit/application_test.csv',
-                omit=[['SK_ID_CURR', 'TARGET'], []], target='TARGET')
+
+    d = Dataset(filepath + 'application_train.csv',
+                filepath + 'application_test.csv',
+                omit=[['TARGET'], []], target='TARGET',
+                additional=additional_data)
 
     print(s.run(d))
